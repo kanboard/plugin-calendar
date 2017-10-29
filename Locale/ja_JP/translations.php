2c7f8ba4b1a72f31fba927271006bd69682b3c2b
torinky <k_tori@me.com>
2017-10-29 13:53:56 +0900

    Update Japanese translation.

--- Locale/ja_JP/translations.php
+++ Locale/ja_JP/translations.php
@@ -2,12 +2,14 @@
 
 return array(
     'Calendar' => 'カレンダー',
-    // 'Calendar settings' => '',
-    // 'Project calendar view' => '',
-    // 'User calendar view' => '',
-    // 'My calendar' => '',
-    // 'Switch to the list view' => '',
-    // 'Show tasks based on the start date' => '',
-    // 'Subtasks time tracking' => '',
+    'Calendar settings' => 'カレンダー設定',
+    'Project calendar view' => 'プロジェクトカレンダー・ビュー',
+    'User calendar view' => 'ユーザーカレンダー・ビュー',
+    'My calendar' => '自分のカレンダー',
+    'Switch to the list view' => 'リストビューに切り替える',
+    'Show tasks based on the start date' => '開始日に基づいてタスクを表示',
+    'Subtasks time tracking' => 'サブタスクの時間トラッキング',
+    'Show tasks based on the creation date' => '作成日に基づいてタスクを表示',
+    'Show subtasks based on the time tracking' => '時間トラッキングに基づいてサブタスクを表示',
 );
 
