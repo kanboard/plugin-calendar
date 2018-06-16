9f30934369e92547c6c05b92bc571665c5286f1e
Balázs Úr <urbalazs@gmail.com>
2018-06-16 18:27:13 +0200

    Updated Hungarian translation

--- Locale/hu_HU/translations.php
+++ Locale/hu_HU/translations.php
@@ -3,11 +3,11 @@
 return array(
     'Calendar' => 'Naptár',
     'Calendar settings' => 'Naptár beállítások',
-    'Project calendar view' => 'A projekt megjelenítése naptári formában',
-    'User calendar view' => 'A felhasználó naptárának megjelenítése',
-    'My calendar' => 'Naptáram',
-    'Switch to the list view' => 'Átkapcsolás lista nézetbe',
-    'Show tasks based on the start date' => 'A feladatok megjelenítése a kezdő dátum alapján',
-    'Subtasks time tracking' => 'A részfeladatok idejének megjelenítése',
+    'Project calendar view' => 'Projektnaptár nézet',
+    'User calendar view' => 'Felhasználónaptár nézet',
+    'My calendar' => 'Saját naptár',
+    'Switch to the list view' => 'Váltás a listanézetre',
+    'Show tasks based on the start date' => 'Feladatok megjelenítése a kezdési dátum alapján',
+    'Subtasks time tracking' => 'Részfeladatok idejének követése',
 );
 
