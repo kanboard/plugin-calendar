d2fcd33295948b8225b5423b39b9876ff65d909b
Slavko <linux@slavino.sk>
2019-03-17 13:04:41 +0100

    Create Slovak translation

---
+++ Locale/sk_SK/translations.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'Calendar' => 'Kalendár',
+    'Calendar settings' => 'Nastavenia kalendára',
+    'Project calendar view' => 'Zobrazenie kalendára projektu',
+    'User calendar view' => 'Zobrazenie kalendára používateľa',
+    'My calendar' => 'Môj kalendár',
+    'Switch to the list view' => 'Prepnúť na zobrazenie zoznamu',
+    'Show subtasks based on the time tracking' => 'Zobraziť podúlohy na základe sledovania času',
+    'Show tasks based on the creation date' => 'Zobraziť úlohy na základe dátumu vytvorenia',
+    'Show tasks based on the start date' => 'Zobraziť úlohy na základe dátumu začatia',
+    'Subtasks time tracking' => 'Sledovanie času podúloh',
+);
