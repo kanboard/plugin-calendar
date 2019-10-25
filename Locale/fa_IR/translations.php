eb110c8ac0defca8f82f6df667df55736afb142b
حمید کرد <mer30hamid@gmail.com>
2019-10-25 05:03:49 +0330

    Add Persian translation

---
+++ Locale/fa_IR/translations.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'Calendar' => 'تقویم',
+    'Calendar settings' => 'تنظیمات تقویم',
+    'Project calendar view' => 'نمای تقویمی پروژه',
+    'User calendar view' => 'نمای تقویمی کاربر',
+    'My calendar' => 'تقویم من',
+    'Switch to the list view' => 'تغییر به نمای لیست',
+    'Show subtasks based on the time tracking' => 'کارهای فرعی را بر اساس پیگیری زمان نشان بده',
+    'Show tasks based on the creation date' => 'کارها را بر اساس تاریخ ایجاد نشان بده',
+    'Show tasks based on the start date' => 'کارها را بر اساس تاریخ شروع نشان بده',
+    'Subtasks time tracking' => 'پیگیری زمان کارهای فرعی',
+);
