3d5dff579170c6d79e97016520fecf9c84d32c70
trendspotter <j.podhorecky@volny.cz>
2020-04-18 17:51:51 +0200

    Update cs_CZ translations

--- Locale/cs_CZ/translations.php
+++ Locale/cs_CZ/translations.php
@@ -3,11 +3,11 @@
 return array(
     'Calendar' => 'Kalendář',
     'Calendar settings' => 'Nastavení kalendáře',
-    // 'Project calendar view' => '',
+    'Project calendar view' => 'Zobrazení kalendáře projektu',
     'User calendar view' => 'Zobrazení kalendáře uživatele',
     'My calendar' => 'Můj kalendář',
     'Switch to the list view' => 'Přepnout na seznam zobrazení',
-    'Show tasks based on the start date' => 'Zobrazit úkoly podle datumu zahájení',
+    'Show tasks based on the start date' => 'Zobrazit úkoly podle data zahájení',
     'Subtasks time tracking' => 'Dílčí úkoly s časovačem',
 );
 
